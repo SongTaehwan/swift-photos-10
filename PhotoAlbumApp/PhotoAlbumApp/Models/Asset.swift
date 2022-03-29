2c52217bf89d154274d227d52b03657987f99b5b
Song TaeHwan <axoghks@gmail.com>
2022-03-29 16:35:46 +0900

    [Add] JSON 디코딩에 사용할 Asset 모델 정의

---
+++ PhotoAlbumApp/PhotoAlbumApp/Models/Asset.swift
@@ -0,0 +1,14 @@
+//
+//  Asset.swift
+//  PhotoAlbumApp
+//
+//  Created by 송태환 on 2022/03/29.
+//
+
+import Foundation
+
+struct Asset: Codable {
+    let title: String
+    let image: String
+    let date: String
+}
